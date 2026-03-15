df5b34f31d35d880eea34568d5f9e6df6b22bdbc
urmit sardhara <sardharaurmit@gmail.com>
2026-03-15 19:05:39 +0530

    fix ui

--- src/TestF.java
+++ src/TestF.java
@@ -1,6 +1,6 @@
 import javafx.application.Application;
 import javafx.geometry.Pos;
-import javafx.scene.Group;
+// import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
