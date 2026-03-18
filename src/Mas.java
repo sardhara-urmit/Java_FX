a1baed7e039189418da4508b12f5ebe6493886e4
urmit sardhara <sardharaurmit@gmail.com>
2026-03-18 22:53:02 +0530

    add file

---
+++ src/Mas.java
@@ -0,0 +1,21 @@
+import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Mas extends Application{
+    @Override
+    public void start(Stage arg0) throws Exception {
+        Button btn1=new Button("click me ");
+        Label lb1 =new Label();
+        VBox root =new VBox(20);
+        root.getChildren().addAll(btn1,lb1);
+
+        arg0.show();
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
