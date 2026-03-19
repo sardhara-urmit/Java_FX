282059997c26d1b75ae03e6e8381f0b68cda3ec0
urmit sardhara <sardharaurmit@gmail.com>
2026-03-19 11:55:09 +0530

    add test file

--- src/Mas.java
+++ src/Mas.java
@@ -1,4 +1,5 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
@@ -7,10 +8,13 @@ import javafx.stage.Stage;
 public class Mas extends Application{
     @Override
     public void start(Stage arg0) throws Exception {
-        Button btn1=new Button("click me ");
-        Label lb1 =new Label();
-        VBox root =new VBox(20);
-        root.getChildren().addAll(btn1,lb1);
+            Button btn1=new Button("click me ");
+            Label lb1 =new Label();
+            VBox root =new VBox(20);
+            root.getChildren().addAll(btn1,lb1);
+
+            Scene scene = new Scene(root,200,200);
+            arg0.setScene(scene);
 
         arg0.show();
     }
---
+++ src/Pro.java
@@ -0,0 +1,15 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Pro extends Application{
+    @Override
+    public void start(Stage arg0) throws Exception {
+        
+        arg0.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
