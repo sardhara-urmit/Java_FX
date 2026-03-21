687811f38b485bc9e3021d8118289f8c3ac0373b
urmit sardhara <sardharaurmit@gmail.com>
2026-03-21 15:35:32 +0530

    set new file

---
+++ src/Master_form.java
@@ -0,0 +1,15 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Master_form extends Application{
+    @Override
+    public void start(Stage arg0) throws Exception {
+
+
+        arg0.show();
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
