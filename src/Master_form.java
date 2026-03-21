2ea4faa447878709a732e891a0405fa88bfdcd0a
urmit sardhara <sardharaurmit@gmail.com>
2026-03-21 15:45:25 +0530

    creat base form model

--- src/Master_form.java
+++ src/Master_form.java
@@ -15,11 +15,16 @@ public class Master_form extends Application{
         TextField userField=new TextField();
         Label lb2= new Label("Password");
         PasswordField pass=new PasswordField();
-        Button submit=new Button();
-        Button reset=new Button();
+        Button submit=new Button("Subimit");
+        Button reset=new Button("reset");
 
         GridPane root=new GridPane();
-        
+        root.add(lb1, 0, 0);
+        root.add(userField, 1, 0);
+        root.add(lb2,0 , 1);
+        root.add(pass, 1, 1);
+        root.add(submit, 0, 2);
+        root.add(reset, 1, 2);
 
         Scene scene=new Scene(root,500,500);
         arg0.setScene(scene);
