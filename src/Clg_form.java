f245721ed7aac95289ad42050a1c103e0486f31b
urmit sardhara <sardharaurmit@gmail.com>
2026-03-24 21:06:10 +0530

    add more conntrol or style

--- src/Clg_form.java
+++ src/Clg_form.java
@@ -1,7 +1,10 @@
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -12,12 +15,35 @@ public class Clg_form extends Application{
         TextField fname =new TextField();
         Label Mname =new Label("Enter middel name :");
         TextField mname =new TextField();
-        Label Lname=new Label("Enter Last name : ");
+        Label Lname=new Label("Enter Last name :");
         TextField lname =new TextField();
+        RadioButton rd1=new RadioButton("Male");
+        RadioButton rd2 =new RadioButton("Female");
+        ToggleGroup group =new ToggleGroup();
+        rd1.setToggleGroup(group);
+        rd2.setToggleGroup(group);
+
 
         GridPane root =new GridPane();
         root.add(Fname, 0, 0);
+        root.add(fname,1,0);
+        root.add(Mname, 0, 1);
+        root.add(mname,1,1);
+        root.add(Lname, 0, 2);
+        root.add(lname,1,2);
+        root.add(rd1, 0, 3);
+        root.add(rd2, 1, 3);
+
         
+
+
+        // style ----
+        root.setAlignment(Pos.CENTER);
+        root.setVgap(10);
+        root.setHgap(10);
+        root.setStyle("-fx-background-color:linear-gradient(to right ,red , blue)");
+
+
 
         Scene scene =new Scene(root,500,500);
         arg0.setScene(scene);
