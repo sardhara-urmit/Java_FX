9fdd144e1210c0b67df08258ebb0897adfb7d2a4
urmit sardhara <sardharaurmit@gmail.com>
2026-03-25 10:25:51 +0530

    add sem using combo box

--- src/Clg_form.java
+++ src/Clg_form.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
@@ -12,23 +13,34 @@ import javafx.stage.Stage;
 public class Clg_form extends Application{
     @Override
     public void start(Stage arg0) throws Exception {
+        // name fild 
         Label Fname= new Label("Enter name : ");
         TextField fname =new TextField();
         Label Mname =new Label("Enter middel name :");
         TextField mname =new TextField();
         Label Lname=new Label("Enter Last name :");
         TextField lname =new TextField();
+        //Gender 
+        Label Gender =new Label("Gender");
         RadioButton rd1=new RadioButton("Male");
         RadioButton rd2 =new RadioButton("Female");
-        Label Gender =new Label("Gender");
+           // Gender (RadioButton)
         ToggleGroup group =new ToggleGroup();
         rd1.setToggleGroup(group);
         rd2.setToggleGroup(group);
+
+          // Hobby (CheckBox)
+
         Label Hobby =new Label("Hobby");
         CheckBox playGame = new CheckBox("play game");
         CheckBox Music =new CheckBox("MUsic");
         CheckBox  Singing =new CheckBox("Singing "); 
 
+        // Semester combo box 
+
+        Label semStr=new Label("Semester");
+        ComboBox<String> sem=new ComboBox<>();
+        sem.getItems().addAll("sem-1","sem-2","sem-3","sem-5","sem-6");
 
 
 
@@ -46,8 +58,9 @@ public class Clg_form extends Application{
         root.add(Hobby, 0, 4);
         root.add(playGame, 1, 4);
         root.add(Music, 2, 4);
-        
-        
+        root.add(Singing, 3, 4);
+        root.add(semStr, 0,5);
+        root.add(sem, 1, 5);     
 
 
         // style ----
@@ -58,7 +71,7 @@ public class Clg_form extends Application{
 
 
 
-        Scene scene =new Scene(root,500,500);
+        Scene scene =new Scene(root,1000,780);
         arg0.setScene(scene);
        arg0.setTitle("Register");
         arg0.show();
