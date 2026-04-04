dd9f886ffc92c327d44baad23f0c1e0c3842d5e5
urmit sardhara <sardharaurmit@gmail.com>
2026-04-04 13:23:26 +0530

    add new img code for test

---
+++ src/T_img.java
@@ -0,0 +1,29 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class T_img extends Application{
+    @Override
+    public void start(Stage arg0) throws Exception {
+        Image img =new Image("file:src\\Mda\\img.jpg");
+        ImageView imageView =new ImageView(img);
+        imageView.setFitHeight(250);
+        imageView.setFitWidth(200);
+
+        GridPane root =new GridPane();
+        root.add(imageView, 0, 0);
+        
+        Scene scene=new Scene(root,500,500);
+        arg0.setScene(scene);
+
+        arg0.show();
+    }
+    
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
