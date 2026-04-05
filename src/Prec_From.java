7a62a751a19f060601e699c1418be3f67a4c782f
urmit sardhara <sardharaurmit@gmail.com>
2026-04-05 17:57:01 +0530

    add new control

--- src/Prec_From.java
+++ src/Prec_From.java
@@ -3,7 +3,9 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
@@ -18,14 +20,35 @@ public class Prec_From extends Application{
 
       TextField Pass =new TextField();
       Pass.setPromptText("enter pass ");
+
+
+
+
+      /// gender 
       RadioButton Male=new RadioButton("Male");
       RadioButton Female =new RadioButton("Female");
       ToggleGroup group =new ToggleGroup();
-    Male.setToggleGroup(group);
-    Female.setToggleGroup(group);
+      Male.setToggleGroup(group);
+      Female.setToggleGroup(group);
 
+    // combo box
+    ComboBox<String> comboBox =new ComboBox<>();
+    comboBox.getItems().addAll("sem-1","sem-2","sem3");
 
-      Button submit =new Button("submit");
+    // list view
+    ListView<String> listView =new ListView<>();
+    listView.setMaxHeight(80);
+    
+    //sub vali
+   comboBox.setOnAction(e->{
+    if (comboBox.getValue().equals("sem-1")) {
+
+        listView.getItems().addAll("hrmlt","css","js");
+    }
+
+   });
+
+    Button submit =new Button("submit");
       Label note =new Label();
 
 
@@ -36,6 +59,8 @@ public class Prec_From extends Application{
       root.add(note, 0, 10);
       root.add(Male, 0, 4);
       root.add(Female, 1, 4);
+      root.add(comboBox, 0, 5);
+      root.add(listView, 0, 6);
 
       Scene scene =new Scene(root,500,500);
       arg0.setScene(scene);
